26ef0d0e891b775e47bfae11912819ac1d6caa1b
Andrew Ma <andrew.ma.1996@gmail.com>
2016-10-13 14:58:12 -0500

    rgbsplit, opening, closing, and merging

--- rgbsplit.py
+++ rgbsplit.py
@@ -12,9 +12,23 @@ red = src[:,:,2]
 green = src[:,:,1]
 blue = src[:,:,0]
 
-plt.subplot(232),plt.imshow(src,'gray'),plt.title('Original')
-plt.subplot(234),plt.imshow(blue,'gray'),plt.title('Blue')
-plt.subplot(235),plt.imshow(green,'gray'),plt.title('Green')
-plt.subplot(236),plt.imshow(red,'gray'),plt.title('Red')
+
+
+kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(5,5))
+openRed = cv2.morphologyEx(red, cv2.MORPH_OPEN, kernel)
+openGreen = cv2.morphologyEx(green, cv2.MORPH_OPEN, kernel)
+openBlue = cv2.morphologyEx(blue, cv2.MORPH_OPEN, kernel)
+
+finalRed = cv2.morphologyEx(red, cv2.MORPH_CLOSE, kernel)
+finalGreen = cv2.morphologyEx(green, cv2.MORPH_CLOSE, kernel)
+finalBlue = cv2.morphologyEx(blue, cv2.MORPH_CLOSE, kernel)
+
+final = cv2.merge([finalBlue, finalGreen, finalRed])
+
+plt.subplot(332),plt.imshow(src,'gray'),plt.title('Original')
+plt.subplot(334),plt.imshow(blue,'gray'),plt.title('Blue')
+plt.subplot(335),plt.imshow(green,'gray'),plt.title('Green')
+plt.subplot(336),plt.imshow(red,'gray'),plt.title('Red')
+plt.subplot(338),plt.imshow(final,'gray'),plt.title('FINAL')
 
 plt.show()
