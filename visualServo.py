8a9071c5493ffc5d56f4777b5a421439f96feca7
Francis Papa <Fnasty@wirelessprv-10-195-177-93.near.illinois.edu>
2017-02-19 16:55:54 -0600

    Visual Servo file

--- visualServo.py
+++ visualServo.py
@@ -2,26 +2,26 @@ import numpy as np
 from numpy.linalg import inv
 import cv
 
-def VisualServo(x, y, z, a, b, c, d, e, f):
+#This function takes a position vector and breaks it down into its individual components
+def SdotBreakdown(positionVector):
+    s1 = positionVector[0]
+    s2 = positionVector[1]
+    return s1, s2
+
+#This function computes the image jacobian matrix given inputs x,y,z (u, v, z) and the feature value matrix S
+def VisualServo(x,y,z,s1,s2):
     L = np.matrix([[(1.0/z), 0.0, -(x/z), -x*y, (1.0 + x**2), -y], [0.0, (1.0/z), -(y/z), -(1.0 + y**2), x*y, x]])
-    Lx = np.matrix([[(1.0/c), 0.0, -(a/c), -a*b, (1.0 + a**2), -b], [0.0, (1.0/c), -(b/c), -(1.0 + b**2), a*b, a]])
-    Ly = np.matrix([[(1.0/f), 0.0, -(d/f), -d*e, (1.0 + d**2), -e], [0.0, (1.0/f), -(e/f), -(1.0 + e**2), d*e, d]])
-    L = np.concatenate((L, Lx, Ly), axis=0)
-    
-    S = np.matrix([[1.0],[2.0],[3.0],[4.0],[5.0],[6.0]])
-   
+    S = np.matrix([[s1],[s2]])
+    return L, S
 
-        Lt = L.transpose()
-        Lp = np.matmul(Lt, L)
-        Linv = inv(Lp)
-        Lp = np.matmul(Linv, Lt)
-        Vc = np.matmul(Lp, S)
-
-
-    return L, S, Vc
-
-L, S, Vc = VisualServo(1.0, 2.0, 3.0, 2.0, 4.0, 3.0, 9.0, 7.5, 8.45)
-print(L, S, Vc)
+#This function solves for camera velocity using the equation Vc = S * ((L^T * L)^-1 * L^T)
+def SolveVisualServo(L,S):
+    Lt = L.transpose()
+    Lp = np.matmul(Lt, L)
+    Linv = inv(Lp)
+    Lp = np.matmul(Linv, Lt)
+    Vc = np.matmul(Lp, S)
+    return Vc
 
 
 
