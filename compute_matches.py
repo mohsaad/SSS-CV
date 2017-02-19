c6c25a7d4cebb3904cb4d744ddf51c8346a40380
Andrew Ma <andrew.ma.1996@gmail.com>
2017-02-19 17:53:11 -0600

    compute_matches.py now calculates pixel velocity and position
    also added up down and output images from compute_matches.py

--- compute_matches.py
+++ compute_matches.py
@@ -9,27 +9,12 @@ import cv2
 
 def drawMatches(img1, kp1, img2, kp2, matches):
     """
-    My own implementation of cv2.drawMatches as OpenCV 2.4.9
-    does not have this function available but it's supported in
-    OpenCV 3.0.0
-
-    This function takes in two images with their associated 
-    keypoints, as well as a list of DMatch data structure (matches) 
-    that contains which keypoints matched in which images.
-
-    An image will be produced where a montage is shown with
-    the first image followed by the second image beside it.
-
-    Keypoints are delineated with circles, while lines are connected
-    between matching keypoints.
-
     img1,img2 - Grayscale images
     kp1,kp2 - Detected list of keypoints through any of the OpenCV keypoint 
               detection algorithms
     matches - A list of matches of corresponding keypoints through any
               OpenCV keypoint matching algorithm
     """
-
     # Create a new output image that concatenates the two images together
     # (a.k.a) a montage
     rows1 = img1.shape[0]
@@ -72,57 +57,59 @@ def drawMatches(img1, kp1, img2, kp2, matches):
     # Also return the image if you'd like a copy
     return out
 
-#import images
-downorig = cv2.imread("down.jpg", cv2.IMREAD_GRAYSCALE)
-uporig = cv2.imread("up.jpg", cv2.IMREAD_GRAYSCALE)
 
-height, width = uporig.shape
-up = cv2.resize(uporig, (int(height * .25), int(width * .25)))
-down = cv2.resize(downorig, (int(height * .25), int(width * .25)))
+def import_images():
+	#import images
+	downorig = cv2.imread("down.jpg", cv2.IMREAD_GRAYSCALE)
+	uporig = cv2.imread("up.jpg", cv2.IMREAD_GRAYSCALE)
 
-'''
-#     HARRIS CORNERS:
-corners = cv2.cornerHarris(down, 2, 3, 0.04)
-corner_down = corners[corners > 0.01 * corners.max()]
+	#resize images to 1/4 of size
+	height, width = uporig.shape
+	up = cv2.resize(uporig, (int(height * .25), int(width * .25)))
+	down = cv2.resize(downorig, (int(height * .25), int(width * .25)))
+	return up, down
 
-corners = cv2.cornerHarris(up, 2, 3, 0.04)
-corner_up = corners[corners > 0.01 * corners.max()]
+def find_keypoints():
+	up, down = import_images()
 
-max_corner_resp = corners > 0.01 * corners.max()
-print(max_corner_resp.shape)
-'''
+	#create the ORB object
+	orb = cv2.ORB()
 
-#ORB
-orb = cv2.ORB()
+	#find keypoints and descriptors
+	ft1, des1 = orb.detectAndCompute(down, None)
+	ft2, des2 = orb.detectAndCompute(up, None)
 
-#keypoints/features
-ft1, des1 = orb.detectAndCompute(down, None)
-ft2, des2 = orb.detectAndCompute(up, None)
+	#BF Matching object
+	bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
 
-#draw features
-downft = cv2.drawKeypoints(down, ft1, None, color=(0,255,0), flags=0)
-upft = cv2.drawKeypoints(up, ft2, None, color=(0,255,0), flags=0)
+	# Match descriptors and create 
+	matches = bf.match(des1,des2)
 
-'''
-#draw the features
-plt.imshow(downft),plt.show()
-plt.imshow(upft),plt.show()
-'''
+	# Sort them in the order of their distance.
+	matches = sorted(matches, key = lambda x:x.distance)
+	return ft1, ft2, matches
 
-#BF Matching object
-bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+def position_vector():
+	posv = np.zeros((5,5))
+	kp1, kp2, matches = find_keypoints()
+	for i in range(0, 5):
 
-# Match descriptors
-matches = bf.match(des1,des2)
+		#find the kp indices
+		idx1 = matches[i].queryIdx
+		idx2 = matches[i].trainIdx
 
-# Sort them in the order of their distance.
-matches = sorted(matches, key = lambda x:x.distance)
+		#get positions
+		(x1, y1) = kp1[idx1].pt
+		(x2, y2) = kp2[idx2].pt
 
-# Draw first 10 matches.
-out = drawMatches(down,ft1,up,ft2,matches[:5])
+		#velocity
+		udot = x2 - x1
+		vdot = y2 - y1
+		posv[i][0] = udot
+		posv[i][1] = vdot
+		posv[i][2] = x2
+		posv[i][3] = y2
+		posv[i][4] = 1
+	print(posv)
 
-cv2.imwrite('out.png', out)
-'''
-cv2.waitKey(0)
-cv2.destroyWindow('Matched Features')
-'''
+position_vector()
